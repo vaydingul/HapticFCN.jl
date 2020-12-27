f9c52717408531837ae6a73ace8c4bc838807d05
Volkan Aydıngül <volkanaydingul@gmail.com>
2020-12-27 17:17:17 +0300

    Small fixes

--- scripts/VisualNet_kuacc.jl
+++ scripts/VisualNet_kuacc.jl
@@ -1,12 +1,12 @@
-push!(LOAD_PATH, "/scratch/users/vaydingul20/workfolder/COMP541_Project/src/modules/")
 using Distributed
 
+@everywhere push!(LOAD_PATH, "/scratch/users/vaydingul20/workfolder/COMP541_Project/src/modules/")
 
-@everywhere using Pkg; Pkg.activate(".")  # required
 @everywhere using Model: HapticNet, VisualNet, train_epoch!, save_as_jld2
 @everywhere using TUM69: load_accel_data, load_image_data
 @everywhere using Preprocess: process_accel_signal, process_image, augment_image
 @everywhere using Utils: kfold, notify!, a_type
+using JLD2, Random
 
 
 
