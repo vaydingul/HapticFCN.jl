c52b4a269aac29f37c2ca36aa6c806c827cf7576
Volkan Aydıngül <volkanaydingul@gmail.com>
2020-12-27 16:19:15 +0300

    Small fixes

--- scripts/HapticNet_kuacc.jl
+++ scripts/HapticNet_kuacc.jl
@@ -6,10 +6,17 @@ using Utils: kfold, notify!, a_type
 
 using JLD2, Random
 
+notify!("Script started!")
+
+
 path = CUDA.functional() ? "/userfiles/vaydingul20/data/new" : "data/new" # path of the main data
 DATA_PATH = isdir(path) && path
 
+notify!("Data reading started!")
+
 X_train, y_train, _, _, material_dict = load_accel_data(DATA_PATH; mode = "normal")
+
+notify!("Preprocessing started!")
 
 X_train, y_train = process_accel_signal(X_train, y_train)
 #X_test, y_test = process_accel_signal(X_test, y_test)
@@ -17,8 +24,12 @@ X_train, y_train = process_accel_signal(X_train, y_train)
 kf = kfold(X_train, y_train, fold = 3, atype = a_type(Float32))
 results = []
 
+notify!("Training started!")
+
 for (ix, dtrn, dtst) in enumerate(kf.folds)
-    
+
+    notify!("Training $ix started!")
+
     hn = HapticNet(; atype = a_type(Float32))
     res = train_epoch!(hn, dtrn, dtst; progress_bar = false, fig = false, info = true, epoch = 3000)
     save_as_jld2(hn, "hn-$ix.jld2")
@@ -26,4 +37,4 @@ for (ix, dtrn, dtst) in enumerate(kf.folds)
     
 end
 
-JLD2.@save "results.jld2" results = results
+JLD2.@save "results_hn.jld2" results = results
--- scripts/VisualNet_kuacc.jl
+++ scripts/VisualNet_kuacc.jl
@@ -5,11 +5,17 @@ using Preprocess: process_accel_signal, process_image, augment_image
 using Utils: kfold, notify!, a_type
 
 using JLD2, Random
-
+notify!("Script started!")
 path = CUDA.functional() ? "/userfiles/vaydingul20/data/new" : "data/new" # path of the main data
 DATA_PATH = isdir(path) && path
 alexnet_PATH = "/scratch/users/vaydingul20/workfolder/COMP541_Project/alexnet.mat"
+
+notify!("Data reading started!")
+
 X_train, y_train, _, _, material_dict = load_image_data(DATA_PATH; mode = "normal")
+
+notify!("Preprocessing started!")
+
 X_train, y_train = augment_image(X_train, y_train)
 X_train, y_train = process_image(X_train, y_train)
 #X_test, y_test = process_accel_signal(X_test, y_test)
@@ -17,7 +23,11 @@ X_train, y_train = process_image(X_train, y_train)
 kf = kfold(X_train, y_train, fold = 3, atype = a_type(Float32))
 results = []
 
+notify!("Training started!")
+
 for (ix, dtrn, dtst) in enumerate(kf.folds)
+    
+    notify!("Training $ix started!")
 
     vn = VisualNet(alexnet_PATH; atype = a_type(Float32))
     res = train_epoch!(vn, dtrn, dtst; progress_bar = false, fig = false, info = true, epoch = 3000)
@@ -26,4 +36,4 @@ for (ix, dtrn, dtst) in enumerate(kf.folds)
     
 end
 
-JLD2.@save "results.jld2" results = results
+JLD2.@save "results_vn.jld2" results = results
