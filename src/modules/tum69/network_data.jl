f7cb3ba4eb3083e56800e6b2a9de7e8b445741c5
Volkan Aydıngül <volkanaydingul@gmail.com>
2021-01-01 22:11:32 +0300

    Update network_data.jl

--- src/modules/tum69/network_data.jl
+++ src/modules/tum69/network_data.jl
@@ -120,14 +120,14 @@ function iterate(nd::NetworkData, state=(0, 0, true))
         next_s2 = s2 + min(nd.batchsize, length(nd.X_) - s2)
 
     # This state is responsible for the data samples, which is one-to-one inherently.
-        next_s1 += next_s2 == length(nd.X_) ? nd.read_count : 0
+        next_s1 = next_s2 == length(nd.X_) ? s1 + nd.read_count : s1 + 0
 
         next_s3 = next_s2 == length(nd.X_) ? true : false
     else
         next_s2 = s2 + nd.batchsize
 
         # This state is responsible for the data samples, which is one-to-one inherently.
-        next_s1 +=  0
+        next_s1 = s1 +  0
     
         next_s3 = false
 
