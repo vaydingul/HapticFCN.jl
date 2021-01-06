8c60aa4f3f26a3b7b51d4bdfdb8815861eb73ace
Volkan Aydıngül <volkanaydingul@gmail.com>
2021-01-07 00:26:12 +0300

    preprocess update

--- tests/preprocess_test2.jl
+++ tests/preprocess_test2.jl
@@ -3,6 +3,7 @@ using TUM69: load_accel_data, load_image_data
 using Preprocess: process_accel_signal, process_image, augment_image    
 using Utils: notify!
 using Augmentor: FlipX, FlipY
+using TimerOutputs
 # xt, yt, _, _ = load_accel_data("data/trial"; mode = "baseline")
 xt_, yt_, _, _ = load_image_data("data/trial"; mode="baseline")
 # xt, yt = process_accel_signal(xt, yt)
@@ -12,12 +13,14 @@ xt_, yt_, _, _ = load_image_data("data/trial"; mode="baseline")
 p1 = FlipX()
 p2 = FlipY()
 p3 = FlipX() |> FlipY()
-xt_, yt_ = augment_image(xt_, yt_, p1, p2, p3)
+@time xt_, yt_ = augment_image(xt_, yt_, p1, p2, p3);
 # notify!("augment")
 
 #@time xt_1, yt_1 = process_image(xt_, yt_)
-@time xt_2, yt_2 = process_image(xt_, yt_);
+reset_timer!()
 
+xt_2, yt_2 = process_image(xt_, yt_);
+print_timer()
 
 #augment(xt_[1], SplitChannels() |> PermuteDims(2,3,1) |> ConvertEltype(Float32))
 
